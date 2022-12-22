b0d531fac66cb2821cbb689cdfe5e690e03c8674
Gopika9496 <121215936+Gopika9496@users.noreply.github.com>
2022-12-22 11:34:58 +0530

    Create bubblesort.c

---
+++ bubblesort.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+void main()
+{
+  int a[30],i,j,n,temp;
+  printf("enter the number of an elements:\n");
+  scanf("%d",&n);
+  printf("enter the array elements:\n");
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+printf("array is....\n");
+ for(i=0;i<n;i++)
+{ 
+  printf("%d\t",a[i]);
+printf("\n");
+}
+ for(i=0;i<n-1;i++)
+ for(j=0;j<n-i-1;j++)
+{
+ if(a[j]>a[j+1])
+   {
+     temp=a[j];
+     a[j]=a[j+1];
+     a[j+1]=temp;
+
+   }
+}
+printf("array after implementing bubble sort is....\n");
+  for(i=0;i<n;i++)
+   printf("%d\t",a[i]);
+printf("\n");
+}
